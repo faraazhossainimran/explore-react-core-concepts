c0e7fdb5fb9db1251669863384a5b63ec4457687
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-09-07 17:54:31 +0600

    conditional rendaring in react using react

---
+++ src/Todo.jsx
@@ -0,0 +1,31 @@
+// export default function Todo ({task, isDone}) {
+    
+//     return (
+//         <li>Task: {task}</li>
+//     )
+// }
+// conditional rendaring (ternary)
+// export default function Todo ({task, isDone}) {
+    
+//     return (
+//         <div>
+//             <li>Task: {task} - {isDone? 'great, work is done' : 'please do the work'}</li>
+//         </div>
+//     )
+// }
+// // conditional rendaring &&
+// export default function Todo ({task, isDone}) {
+    
+//     return (
+//         <li>Task: {task} {isDone && ': Great work is done'}</li>
+//     )
+// }
+
+
+// conditional rendaring ||
+// export default function Todo ({task, isDone}) {
+    
+//     return (
+//         <li>Task: {task} {isDone || ': do it'}</li>
+//     )
+// }
