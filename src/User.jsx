43fc491fc79cc809768c0dafba66acfc9941ae8f
Dev Enthusiastic <faraazhossainimran@gmail.com>
2023-09-08 13:03:28 +0600

    state basis

--- src/User.jsx
+++ src/User.jsx
@@ -1,6 +1,8 @@
+import { useState } from 'react'
 import './User.css'
-export default function User ({user}){
+export default function User ({user, count,handlePlus, handleMinus}){
     const {email, id, name, phone, username, website} = user
+
     return (
         <div className="user">
             <p>Id: {id}</p>
@@ -9,6 +11,9 @@ export default function User ({user}){
             {/* <p>{phone}</p> */}
             <p>{username}</p>
             <p>{website}</p>
+            <p>Count: {count}</p>
+            <button onClick={handlePlus}>count plus</button>
+            <button onClick={handleMinus}>count minus</button>
         </div>
     )
 }
--- src/Users.jsx
+++ src/Users.jsx
@@ -2,6 +2,15 @@ import { useEffect, useState } from "react"
 import User from "./User";
 import "./Users.css";
 export default function Users () {
+    const [count, setCount] = useState(0)
+    const handlePlus = () => {
+        const newCOunt = count + 1;
+        setCount(newCOunt)
+    }
+    const handleMinus = () => {
+        const newCOunt = count - 1;
+        setCount(newCOunt)
+    }
     const [users, setUsers] =useState([]);
     useEffect(()=>{
         fetch('https://jsonplaceholder.typicode.com/users')
@@ -11,7 +20,7 @@ export default function Users () {
     return (
         <div className="users">
             <h1>{console.log(users)}</h1>
-            {users.map((user)=> <User user={user}></User>)}
+            {users.map((user)=> <User user={user} count={count} handlePlus={handlePlus} handleMinus={handleMinus}></User>)}
         </div>
     )
 }
